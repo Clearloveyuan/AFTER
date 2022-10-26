43631daaea4a6aacf5159f8a17706a4ba28a3f20
Dongyuan.Li <lidy94805@gmail.com>
2022-10-26 10:26:47 +0900

    Update TAPT_full_train.sh

--- bin/TAPT_full_train.sh
+++ bin/TAPT_full_train.sh
@@ -7,4 +7,4 @@ cp Dataset/IEMOCAP/labels_sess/label_2.json Dataset/IEMOCAP/labels_sess_new/
 mkdir -p $3/labels
 cp $2 $3/labels/
 w2v2_path="output_iemocap_train/last.ckpt";
-CUDA_VISIBLE_DEVICES=$4 python run_downstream_custom_multiple_fold.py --precision 16 --num_exps $6 --datadir $1 --labeldir $3/labels --pretrained_path $w2v2_path --outputfile $3/$5.log --inits_method $7
+CUDA_VISIBLE_DEVICES=$4 python run_downstream_custom_multiple_fold.py --precision 16 --num_exps $6 --datadir $1 --labeldir $3/labels --pretrained_path $w2v2_path --outputfile $3/$5.log --inits_method $7 --active_learning $9
